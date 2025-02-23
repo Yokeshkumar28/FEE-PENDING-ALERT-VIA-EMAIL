3a4682712ea72a385f18dfdaa88b001407e332df
Yokeshkumar28 <130818416+Yokeshkumar28@users.noreply.github.com>
2025-02-23 14:00:43 +0530

    Add files via upload

---
+++ Project on Modules (Fee Pending alert via Email)/module project result.py
@@ -0,0 +1,47 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/HAVE A NICE DAY/OneDrive/Desktop/Python Codegnan/Project on Modules (Fee Pending alert via Email)/main module.py
+Enter User name: admin
+Enter OTP: 3437
+Login Sucess !
+Welcome Admin
+Choose your Option
+1. Edit Information
+2. Send mail to Fee Pending users
+3. Send mail to Fee cleared users
+4. Exit
+Enter option: 1
+Enter the Status of yokesh1: false
+yokesh1 Data Updated !
+Enter the Status of yokesh2: false
+yokesh2 Data Updated !
+Enter the Status of srinivas: false
+srinivas Data Updated !
+Data Edited
+Choose your Option
+1. Edit Information
+2. Send mail to Fee Pending users
+3. Send mail to Fee cleared users
+4. Exit
+Enter option: 2
+Mail sent to yokesh1
+Mail sent to yokesh2
+Mail sent to srinivas
+All mails sent to Fee pending users
+Choose your Option
+1. Edit Information
+2. Send mail to Fee Pending users
+3. Send mail to Fee cleared users
+4. Exit
+Enter option: 3
+Mail sent to mahalaxmi
+All mails sent to fee Cleared users !
+Choose your Option
+1. Edit Information
+2. Send mail to Fee Pending users
+3. Send mail to Fee cleared users
+4. Exit
+Enter option: 4
+Thank You
+visit again
